a9d8ddc0a3a30261f9429676dca04250e2e36dd5
SayanMaji <79317924+SayanMaji@users.noreply.github.com>
2021-05-08 16:21:30 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,41 @@
+class Box{
+  constructor(x, y, width, height) {
+      var options = {
+        
+          'restitution':0.8,
+          'friction':1.0,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos=this.body.position;
+      push();
+     // translate(this.body.position.x, this.body.position.y);
+      rectMode(CENTER);
+      var rand=Math.round(random(1,4));
+      switch(rand){
+        case 1: fill("red");
+        break;
+        case 2:fill("blue");
+        break;
+        case 3: fill("yellow");
+        break;
+        case 4: fill("orange");
+        break;
+        default : fill("white");
+      }
+      console.log(this.body.speed);
+      if((this.body.speed)<3)
+      { 
+        rect(pos.x,pos.y,this.width,this.height);}
+      else
+      {
+      World.remove(world,this.body)  
+          }
+      pop();
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,137 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+function preload()
+{
+	getTime();
+}
+
+function setup() {
+	createCanvas(1500, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+    ground1 = new Ground(100,650,3000,30);
+	ground2 = new Ground(785,400,800,5);
+    
+    //1st row
+	box1 = new Box(780,182,50,70);
+	box2 = new Box(805,182,50,70);
+    box3 = new Box(830,182,50,70);
+    box4 = new Box(855,182,50,70);
+    box5 = new Box(880,182,50,70);
+    box6 = new Box(905,182,50,70);
+
+    //2nd row
+    box7 = new Box(795,147,50,70);
+    box8 = new Box(820,147,50,70);
+    box9 = new Box(845,147,50,70);
+    box10 = new Box(870,147,50,70);
+    box11 = new Box(895,147,50,70);
+
+    //3rd row
+    box12 = new Box(805,112,50,70);
+    box13 = new Box(830,112,50,70);
+    box14 = new Box(855,112,50,70);
+    box15 = new Box(880,112,50,70);
+
+    //4th row
+    box16 = new Box(820,77,50,70);
+    box17 = new Box(845,77,50,70);
+    box18 = new Box(870,77,50,70);
+
+    //5th row
+    box19 = new Box(735,42,50,70);
+    box20 = new Box(760,42,50,70);
+
+    box21 = new Box(850,9.5,40,60);
+    
+    striker = new Polygon(150,430,50);
+    shooter = new Launcher(striker.body,{x:190,y:300});
+    
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+	Engine.update(engine)
+  rectMode(CENTER);
+  background(bg);
+  ground1.display();
+ ground2.display();
+
+  //displaying 1st row
+  box1.display();
+  box2.display();
+  box3.display();
+  box4.display();
+  box5.display();
+  box6.display();
+
+  //displaying 2nd row
+  box7.display();
+  box8.display();
+  box9.display();
+  box10.display();
+  box11.display();
+
+  //displaying 3rd row
+  box12.display();
+  box13.display();
+  box14.display();
+  box15.display();
+
+  //displaying 4th row
+  box16.display();
+  box17.display();
+  box18.display();
+
+  //displaying 5th row
+  box19.display()
+  box20.display();
+
+  box21.display();
+  striker.display();
+  shooter.display();
+ 
+}
+function mouseDragged(){
+    Matter.Body.setPosition(striker.body, {x: mouseX , y: mouseY});
+}
+
+
+function mouseReleased(){
+    shooter.fly();
+}
+function keyPressed(){
+    if(keyCode===32)
+    shooter.attach(striker.body);
+}
+async function getTime(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
+console.log(responseJSON)
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+    console.log(datetime);
+    console.log(hour);
+    if(hour>=06 && hour<=17){
+        bg = "download.jpeg";
+    }
+    else{
+        bg = "down.jpg";
+    }
+
+    backgroundImg = loadImage(bg);
+}
+
